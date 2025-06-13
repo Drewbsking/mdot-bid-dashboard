2237f970213bff552d432b2b5748b5d3f208f91f
Bates <abates@rcoc.org>
2025-06-13 08:20:31 -0400

    add counties

--- app.py
+++ app.py
@@ -11,6 +11,39 @@ def load_data():
 df = load_data()
 df["Letting Date"] = pd.to_datetime(df["Letting Date"]).dt.date
 
+# --- County Filter (All counties, defaulting to SEMCOG + Lapeer + Genesee) ---
+county_map = {
+    "Alcona": 1, "Alger": 2, "Allegan": 3, "Alpena": 4, "Antrim": 5, "Arenac": 6, "Baraga": 7, "Barry": 8,
+    "Bay": 9, "Benzie": 10, "Berrien": 11, "Branch": 12, "Calhoun": 13, "Cass": 14, "Charlevoix": 15,
+    "Cheboygan": 16, "Chippewa": 17, "Clare": 18, "Clinton": 19, "Crawford": 20, "Delta": 21, "Dickinson": 22,
+    "Eaton": 23, "Emmet": 24, "Genesee": 25, "Gladwin": 26, "Gogebic": 27, "Grand Traverse": 28, "Gratiot": 29,
+    "Hillsdale": 30, "Houghton": 31, "Huron": 32, "Ingham": 33, "Ionia": 34, "Iosco": 35, "Iron": 36,
+    "Isabella": 37, "Jackson": 38, "Kalamazoo": 39, "Kalkaska": 40, "Kent": 41, "Keweenaw": 42, "Lake": 43,
+    "Lapeer": 44, "Leelanau": 45, "Lenawee": 46, "Livingston": 47, "Luce": 48, "Mackinac": 49, "Macomb": 50,
+    "Manistee": 51, "Marquette": 52, "Mason": 53, "Mecosta": 54, "Menominee": 55, "Midland": 56,
+    "Missaukee": 57, "Monroe": 58, "Montcalm": 59, "Montmorency": 60, "Muskegon": 61, "Newaygo": 62,
+    "Oakland": 63, "Oceana": 64, "Ogemaw": 65, "Ontonagon": 66, "Osceola": 67, "Oscoda": 68, "Otsego": 69,
+    "Ottawa": 70, "Presque Isle": 71, "Roscommon": 72, "Saginaw": 73, "St. Clair": 74, "St. Joseph": 75,
+    "Sanilac": 76, "Schoolcraft": 77, "Shiawassee": 78, "Tuscola": 79, "Van Buren": 80, "Washtenaw": 81,
+    "Wayne": 82, "Detroit City": 83, "Wexford": 84
+}
+
+default_counties = [
+    "Livingston", "Macomb", "Monroe", "Oakland", "St. Clair", "Washtenaw", "Wayne", "Lapeer", "Genesee"
+]
+
+county_names = sorted(county_map.keys())
+selected_counties = st.multiselect(
+    "Select Counties (based on Proposal ID):",
+    options=county_names,
+    default=default_counties
+)
+
+if selected_counties:
+    county_codes = [f"{county_map[c]:02d}" for c in selected_counties]
+    df = df[df["Proposal ID"].astype(str).str[:2].isin(county_codes)]
+
+
 # --- Start Page ---
 st.title("MDOT Pay Item Price Explorer")
 
